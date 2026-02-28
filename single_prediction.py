5056f35385a892b7fe78e25e12ef3e3c26062bcb
Adura Kinoshi <51264953+pkinoshi@users.noreply.github.com>
2026-02-28 21:40:15 +0100

    Update single_prediction.py
    Displayed Segmentation Logic.

--- single_prediction.py
+++ single_prediction.py
@@ -30,3 +30,11 @@ def single_prediction_page(model):
         
         st.subheader(f"Result: Cluster {cluster}.")
         st.write(f"This customer has been categorized into **Group {cluster}**.")
+        
+        st.markdown("""|Cluster|Persona|Financial Context| Age & Engagement Profile|
+|----|-----|-------|--------|
+|Cluster 1(Red)| Middle-Aged Centrists| Mid Income / Mid Spending| The most populous group (Age 40 - 60). Their spending is moderate and predictable.|
+|Cluster 2(Blue)| Established High Spenders| High Income / High Spending| Age 30 - 39. They balance high earning power with high consumption.|
+|Cluster 3(Green)| Trend Chasers| Low Income / High Spending| Likely young spenders. They probably have the highest engagement and are likely the primary drivers of trend-based revenue.|
+|Cluster 4 (Orange)| High earners/Mature Savers| High Income/Low Spending| High-earning individuals. Despite their wealth, they show the lowest spending engagement.|
+|Cluster 5 (Purple)| The Budget-Conscious| Low Income / Low Spending| Probably youngsters starting out in the workforce or retired seniors on fixed income.|""")
